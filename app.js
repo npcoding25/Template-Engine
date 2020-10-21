ee46e66f1691b178ddd89d94493a8d0ba5276c17
Nathan Polomark <polomarkn@hotmail.com>
2020-10-21 14:27:32 -0700

    Fixed manager class to pass test. Comments and console.log added to main

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let userId = 1
 
 async function main() {
     console.log("Welcome to the only place you can build a software engineering team in less then 5 minutes. Have fun!")
+    
     const managerInfo = await inquirer.prompt([
         {
             type: "input",
@@ -39,9 +40,10 @@ async function main() {
     ])
     const manager = new Manager ( managerInfo.name, userId++, managerInfo.email, managerInfo.office )
     teamList.push(manager)
-    console.log(teamList)
 
-    for(let i=0; i<teamMembers.length; i++) {
+    console.log(`Saving manager:  ${managerInfo.name}`)
+
+    for(let i=0; i<managerInfo.teamMembers; i++) {
         let employeeType = await inquirer.prompt([
             {
                 type: "list",
@@ -51,7 +53,7 @@ async function main() {
             }
         ])
         employeeType = employeeType.type
-
+      
         const employeeInfo = await inquirer.prompt([
             {
                 type: "input",
@@ -61,20 +63,27 @@ async function main() {
             {
                 type: "input",
                 name: "email",
-                message: `What is ${employeeInfo.name}'s email?`
+                message: `What is the ${employeeType}'s email?`
             },
             {
                 type: "input",
                 name: "info",
-                message: `What is ${employeeInfo.name}'s ${employeeType=="Engineer" ? "Github" : "School"}`
+                message: `What is the ${employeeType}'s ${employeeType=="Engineer" ? "Github" : "School"}`
             }
         ])
         const employee = employeeType=="Engineer"
             ? new Engineer ( employeeInfo.name, userId++, employeeInfo.email, employeeInfo.info )
             : new Intern ( employeeInfo.name, userId++, employeeInfo.email, employeeInfo.info )
         teamList.push(employee)
+
+        console.log(`${employeeInfo.name} has been added to the team. There are now ${teamList.length} members in your team.`)
     }
 
+    if( !fs.existsSync(OUTPUT_DIR) ) fs.mkdirSync(OUTPUT_DIR)
+
+    fs.writeFileSync(outputPath, render(teamList), "utf-8");
+
+    console.log( `Finished writing to: ${outputPath}`)
 }
 main()
 // Write code to use inquirer to gather information about the development team members,
